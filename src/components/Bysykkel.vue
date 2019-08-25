a1c3ec64994398a3fbec07d8479029bc9bef01f0
Tobias Wulvik <tobias.wulvik@gmail.com>
2019-08-25 13:23:06 +0200

    bysykling

---
+++ src/components/Bysykkel.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="bysykkel">{{ info }}</div>
+</template>
+
+<script>
+import axios from "axios";
+
+const lademoparkenId = 6;
+
+export default {
+  name: "bysykkel",
+  data: function() {
+    return {
+      sykler: 0,
+      plass: "",
+      info: ""
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        "https://gbfs.urbansharing.com/trondheimbysykkel.no/station_status.json"
+      )
+      .then(response => (this.info = response));
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
